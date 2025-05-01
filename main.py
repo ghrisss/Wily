35e60f4b92ffa5b98f7ef83ff05d1adca5e863d2
ghrisss <heriquebati@gmail.com>
2025-05-01 22:05:48 +0000

    chore: roteiro planejando acoes de desenvolvimento e criacao de txt de requirements para libs externas

---
+++ main.py
@@ -0,0 +1,23 @@
+## extração obtenção dos dados
+# importacao de bibliotecas e banco de dados
+
+## ajustes/limpeza dos dados
+# análise (inicial) de quais colunas serão úteis
+# tratar valores vazios
+# verificar tipos de dados em cada coluna
+
+## análise exploratória / tratar outliers
+# dados numericos Contínuos e Discretos, dados em texto (categorias), dados em lista, dados em map
+# encoding -> dados True e False, dados categoria de texto
+
+## modelagem + algorítmos
+# metricas de avaliacao
+# definicao do tipo de modelo
+# escolha dos modelos a testar
+# treino e avaliacao
+
+## interpretação dos resultados
+# análise do melhor modelo
+# ajustes e melhorias do melhor modelo
+
+## deploy/producao
